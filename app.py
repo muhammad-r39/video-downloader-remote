c509b11b82c7d80b587281649004b1eafb65c858
Muhammad Russell <muhammadrussell39@gmail.com>
2024-11-25 20:10:42 +0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+import os
+
+app = Flask(__name__)
+
+# Directory to save downloaded videos
+DOWNLOAD_DIR = "downloads"
+if not os.path.exists(DOWNLOAD_DIR):
+    os.makedirs(DOWNLOAD_DIR)
+
+@app.route('/download', methods=['POST'])
+def download_video():
+    try:
+        data = request.get_json()
+        url = data.get('url')
+        if not url:
+            return jsonify({"error": "URL is required"}), 400
+
+        # yt-dlp options
+        ydl_opts = {
+            'outtmpl': f'{DOWNLOAD_DIR}/%(title)s.%(ext)s',
+            'format': 'best',
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+
+        return jsonify({"message": "Download successful", "file": filename}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
